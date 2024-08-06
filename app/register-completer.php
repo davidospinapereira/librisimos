7d3e692138b22c5f81045805a70803bff618638a
davidospinapereira <dao.star@gmail.com>
2024-08-06 00:07:17 -0500

    Commit revertido por fallas en el login luego de los últimos cambios

---
+++ app/register-completer.php
@@ -0,0 +1,5 @@
+<?php
+    /* Esta página replicará la página de editor de perfil casi completamente */
+    // Por ahora, probemos haciendo un simple echo
+    echo "Página de registro alcanzada";
+?>
